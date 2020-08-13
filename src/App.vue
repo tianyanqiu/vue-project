3906e8653e1836a0d734f9b2ade2bc26136c8457
tianyanqiu <745916729@qq.com>
2020-08-13 09:49:18 +0800

    chore: 添加钩子函数的参数示例

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,8 @@
     </a>
 
     <input v-focus />
+
+    <div id="hook-arguments-example" v-demo:foo.a.b="message"></div>
   </div>
 </template>
 
@@ -71,6 +73,7 @@ export default {
         author: "Jane Doe",
         publishedAt: "2016-04-10",
       },
+      message: "hello!",
     };
   },
   directives: {
@@ -78,6 +81,30 @@ export default {
       // 指令的定义
       inserted: function (el) {
         el.focus();
+      },
+    },
+
+    demo: {
+      bind: function (el, binding, vnode) {
+        var s = JSON.stringify;
+        el.innerHTML =
+          "name: " +
+          s(binding.name) +
+          "<br>" +
+          "value: " +
+          s(binding.value) +
+          "<br>" +
+          "expression: " +
+          s(binding.expression) +
+          "<br>" +
+          "argument: " +
+          s(binding.arg) +
+          "<br>" +
+          "modifiers: " +
+          s(binding.modifiers) +
+          "<br>" +
+          "vnode keys: " +
+          Object.keys(vnode).join(", ");
       },
     },
   },
